59b252bd91d47943c0d67f640457b4d416a8544a
ratwater <jbride2001@yahoo.com>
2014-07-08 19:15:32 -0600

    now testing consumption distribution

--- mdb/src/main/java/com/redhat/gpe/tools/TestMDB.java
+++ mdb/src/main/java/com/redhat/gpe/tools/TestMDB.java
@@ -18,15 +18,24 @@ import org.slf4j.LoggerFactory;
 @MessageDriven(name = "LabMDB", activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
     @ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/queue/GPE.SEND"),
+    @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1")
 })
 public class TestMDB implements MessageListener {
 
+    public final static String SLEEP_TIME_MILLIS = "SLEEP_TIME_MILLIS";
+
     private Logger log = LoggerFactory.getLogger("LabMDB");
+    private int sleepTime = 0;
+
+    public TestMDB() {
+        sleepTime = Integer.parseInt(System.getProperty(SLEEP_TIME_MILLIS, "0"));
+    }
 
     public void onMessage(final Message message) {
         try{
             String payload = ((BytesMessage)message).readUTF();
-            log.info("onMessage() payload = "+payload);
+            log.info("onMessage() payload = "+payload+" : sleeping for following millis: "+sleepTime);
+            Thread.sleep(sleepTime);
         }catch(Exception x) {
             x.printStackTrace();
         }
